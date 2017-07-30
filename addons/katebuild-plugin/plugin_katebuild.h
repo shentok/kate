c693f4958f02847f635c52808610ce195ebf1527
Bernhard Beschow <bbeschow@cs.tu-berlin.de>
2017-07-30 14:26:39 +0200

    simplify

--- addons/katebuild-plugin/plugin_katebuild.h
+++ addons/katebuild-plugin/plugin_katebuild.h
@@ -140,7 +140,6 @@ class KateBuildView : public QObject, public KXMLGUIClient, public KTextEditor::
         QStack<QString>   m_make_dir_stack;
         const QRegularExpression m_filenameDetector;
         const QRegularExpression m_filenameDetectorIcpc;
-        bool              m_filenameDetectorGccWorked;
         const QRegularExpression m_newDirDetector;
         unsigned int      m_numErrors;
         unsigned int      m_numWarnings;
